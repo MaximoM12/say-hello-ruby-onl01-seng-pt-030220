dbaffc0ed858ae32556dee21e14df35b01a4431e
Joseph Maximo Marguello <marguellomax@gmail.com>
2020-03-05 16:39:06 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -4,7 +4,7 @@ class Say_hello
   
 def say_hello(name = nil)
  if name  
-   puts "Hello #{name} welcom to Ruby."
+   puts "Hello #{name} welcom to Ruby!"
  else 
    puts "Hello Ruby programer!"
 end
